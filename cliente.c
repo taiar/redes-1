5b8bbbff4e8f60f0cd09098fda3d4d4b9c2a1b2c
André Taiar <andre.taiar@gmail.com>
2016-09-18 17:03:10 -0300

    Cliente básico.

--- cliente.c
+++ cliente.c
@@ -1,7 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char const *argv[]) {
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-  return 0;
+#define BUFSZ 1024
+
+void logexit(const char *str)
+{
+	perror(str);
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+	int s;
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	if(s == -1) logexit("socket");
+
+	struct in_addr addr;
+	if(inet_pton(AF_INET, argv[1], &addr) < 1) logexit("pton");
+
+	struct sockaddr_in dst;
+
+	dst.sin_family = AF_INET;
+	dst.sin_port = htons(atoi(argv[2]));
+	dst.sin_addr = addr;
+
+	struct sockaddr *sa_dst = (struct sockaddr *)&dst;
+
+	if(connect(s, sa_dst, sizeof(*sa_dst))) logexit("connect");
+
+	char line[BUFSZ];
+
+	printf("input> ");
+
+	fgets(line, BUFSZ, stdin);
+	printf("send %s", line);
+	if(send(s, line, strlen(line)+1, 0) <= 0) logexit("send");
+	printf("[waiting response]\n");
+
+	int cnt = 0;
+	while(1) {
+		int r = recv(s, line+cnt, BUFSZ-cnt, 0);
+		if(r == 0) break;
+		if(r < 0) logexit("recv");
+		cnt += r;
+	}
+	printf("recv %s", line);
+
+	close(s);
+	exit(EXIT_SUCCESS);
 }
